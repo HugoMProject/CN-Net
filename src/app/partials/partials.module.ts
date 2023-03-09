20b90a6e5a1f3a9568e93080f9b54adea249edb2
HugoMProject <hugomoyano1996@gmail.com>
2023-03-09 20:09:34 -0300

    estilos agregados al header

--- src/app/partials/partials.module.ts
+++ src/app/partials/partials.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { MaterialModule } from '../shared/material/material.module';
 
 
 @NgModule({
@@ -11,7 +11,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports:[
     HeaderComponent,
